4aa99e530aa7cbb800d1eb7ae33f68d40ded9bf6
Adam Smeal <ASmeal@petsmart.com>
2019-09-26 10:03:11 -0700

    Add project files.

---
+++ Smealio.Grpc.HelloWorld.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using Smealio.Grpc.HelloWorld.App1;
+using System;
+using System.Threading.Tasks;
+
+namespace Smealio.Grpc.HelloWorld.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var app1Service = GrpcChannel.ForAddress("");
+            var client = new Greeter.GreeterClient(app1Service);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Adam" });
+
+           
+        }
+    }
+}
